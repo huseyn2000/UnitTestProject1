ddd95b0762a41545c9a19b382216aef8ce71a8f2
huseyn2000 <huseyn.naghiyev@ufaz.az>
2022-12-01 01:42:56 +0400

    first commit

---
+++ UnitTestProject1/Opearations.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    internal class Opearations
+    {
+
+        public int factorial(int num)
+        {
+
+            int res = 1;
+
+            for (int i = 1; i <= num; i++)
+            {
+
+                res *= i;
+
+            }
+
+            return res;
+
+        }
+
+        public int power(int bas, int pow)
+        {
+            int res = 1;
+
+            for (int i = 1; i <= pow; i++)
+            {
+                res *= bas;
+
+            }
+            return res;
+
+
+        }
+
+        public string tesrAd(string name1)
+        {
+            string res = "";
+
+            for (int i = name1.Length - 1; i >= 0; i--)
+            {
+                res += name1[i];
+            }
+
+            return res;
+        }
+
+
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            //Arrange
+
+            Opearations opearations = new Opearations();
+            int x = 5;
+            int y = 120;
+
+
+            int z = 2;
+            int quvvet = 3;
+            int netice = 8;
+
+
+            string ad1 = "qli";
+            string gozlenilen = "ila";
+
+            //Act
+            int y1 = opearations.factorial(x);
+            int y2 = opearations.power(z, quvvet);
+            string yoxlanmali = opearations.tesrAd(ad1);
+
+            //Assert
+
+            Assert.AreEqual(y, y1);
+            Assert.AreEqual(y2, netice);
+          
+            Assert.AreEqual(yoxlanmali, gozlenilen);
+
+
+
+        }
+    }
+}
